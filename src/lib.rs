50deb974d4d5795183b10336b5eb875e588f9827
swiftaff <swiftaff@gmail.com>
2022-06-03 21:40:43 +0800

    lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("missing filename argument");
+        }
+        let filename = args[1].clone();
+        Ok(Config { filename })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(&config.filename)?;
+    println!(
+        "Reading contents of filename: {:?}\n----------\n{}",
+        &config.filename, contents
+    );
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use rustlang::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,31 +10,8 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) = run(config) {
+    if let Err(e) = rustlang::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
     }
-}
-
-struct Config {
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 2 {
-            return Err("missing filename argument");
-        }
-        let filename = args[1].clone();
-        Ok(Config { filename })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(&config.filename)?;
-    println!(
-        "Reading contents of filename: {:?}\n----------\n{}",
-        &config.filename, contents
-    );
-    Ok(())
 }
