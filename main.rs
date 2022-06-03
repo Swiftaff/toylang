c2634c8ce430d7015564f08f52d6c591ddc64cff
swiftaff <swiftaff@gmail.com>
2022-06-03 21:25:08 +0800

    first commit

---
+++ main.rs
@@ -0,0 +1,33 @@
+use std::env;
+use std::fs;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    let contents =
+        fs::read_to_string(&config.filename).expect("Something went wrong reading the file");
+
+    println!(
+        "Reading contents of filename: {:?}\n----------\n{}",
+        &config.filename, contents
+    );
+}
+
+struct Config {
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err!("missing filename argument");
+        }
+        let filename = args[1].clone();
+        Ok(Config { filename })
+    }
+}
