71a9e3ccb43f2a1dbf5a3742b1a0c7d2315f16b0
swiftaff <swiftaff@gmail.com>
2023-04-22 23:25:42 +0800

    docs fix index

--- docs.bat
+++ docs.bat
@@ -1,2 +1,2 @@
 Xcopy target\\doc docs\\ /S /Q /Y
-echo|set /p="<!DOCTYPE html><html><head></head><body><script>window.location.href = "../docs/toylang/index.html"</script></body></html>" > docs/index.html
+echo|set /p="<!DOCTYPE html><html><head></head><body><script>window.location.href = "toylang/index.html"</script></body></html>" > docs/index.html
