150e25cbda8a1eee936d6c3a13ea36a2833d256e
swiftaff <swiftaff@gmail.com>
2023-04-25 13:25:28 +0800

    WIP proc_macro

--- toylang_macros/src/lib.rs
+++ toylang_macros/src/lib.rs
@@ -1,8 +1,76 @@
-extern crate proc_macro;
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::parse::{Error, Parse, ParseStream};
+use syn::{parse_macro_input, token::Struct, DeriveInput, LitStr};
+
+//use syn::{parse_macro_input, Ident, LitStr};
+//use proc_macro::{Span, TokenStream};
+
+struct DocTest {
+    fn_name: syn::Ident,
+    arg1: syn::LitStr,
+    arg2: syn::LitStr,
+}
+
+impl Parse for DocTest {
+    fn parse(input: ParseStream) -> Result<Self, Error> {
+        let fn_name = input.parse()?;
+        input.parse::<syn::Token![,]>()?;
+        let arg1 = input.parse()?;
+        input.parse::<syn::Token![,]>()?;
+        let arg2 = input.parse()?;
+        Ok(DocTest {
+            fn_name,
+            arg1,
+            arg2,
+        })
+    }
+}
 
 #[proc_macro]
-pub fn make_answer(item: TokenStream) -> TokenStream {
-    println!("###{:?}", item);
-    "fn answer() -> u32 { 42 }".parse().unwrap()
+pub fn example_proc_macro(input: TokenStream) -> TokenStream {
+    let DocTest {
+        fn_name,
+        arg1,
+        arg2,
+    } = parse_macro_input!(input as DocTest);
+    //dbg!(input_str);
+    let output = quote! {
+        pub fn #fn_name() -> String {
+            format!("{}{}", #arg1, #arg2)
+        }
+    };
+    output.into()
 }
+
+/*
+#[proc_macro]
+pub fn example_proc_macro(input: TokenStream) -> TokenStream {
+    // Parse the input tokens as a string literal
+    let input_str = parse_macro_input!(input as LitStr);
+    dbg!(&input_str);
+
+    // Extract the function name and the two string literals
+    //let val = input_str.value();
+    //dbg!(&val);
+    //let input_parts: Vec<_> = val.split(',').collect();
+    //dbg!(&input_parts);
+    //let fn_name = input_parts[0].trim();
+    //dbg!(&fn_name);
+    //let name: Ident = Ident::new(&val, Span::call_site().into());
+
+    //let greeting = input_parts[1].trim();
+    //let subject = input_parts[2].trim();
+
+    // Generate the function code
+    let output = quote! {
+        //pub fn #name() -> String {
+        //    "testing".to_string()
+            //println!("{}{}!", #greeting, #subject);
+        //}
+    };
+    dbg!(&output);
+
+    output.into()
+}
+*/
--- toylang_macros/tests/test_macros.rs
+++ toylang_macros/tests/test_macros.rs
@@ -1,10 +1,41 @@
 #[cfg(test)]
 mod tests {
+    use toylang_macros::example_proc_macro;
 
-    use toylang_macros::make_answer;
+    struct Testy {
+        fn_name: String,
+        comment1: String,
+        comment2: String,
+    }
+
+    impl Testy {
+        pub fn new(name: &str, comment1: &str, comment2: &str) -> Testy {
+            Testy {
+                fn_name: name.to_string(),
+                comment1: comment1.to_string(),
+                comment2: comment2.to_string(),
+            }
+        }
+    }
 
     #[test]
     fn test_macro() {
-        assert_eq!(String::from(make_answer!("testy")), "test".to_string());
+        let v = Testy::new("name_of_function", "a", "b");
+        example_proc_macro!(testy, "te", "st");
+
+        //example_proc_macro!(v);
+
+        assert_eq!(testy(), "test");
     }
 }
+
+/*
+#[test]
+fn test_macro() {
+    //"test_pass_empty_file" "" "fn main() {\r\n}\r\n"
+    //let s = Testy::new("testymctestface");
+    //let v = vec!["a".to_string(), "b".to_string()];
+    //assert_eq!(name_of_function(), "testing");
+    assert_eq!(true, true);
+}
+*/
