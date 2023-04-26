dae7a212d65dc595c6664d094ae0c334698fc67b
swiftaff <swiftaff@gmail.com>
2023-04-26 23:11:25 +0800

    move remaining tests out of parse

--- toylang_common/src/lib.rs
+++ toylang_common/src/lib.rs
@@ -90,13 +90,18 @@ impl IntegrationTests {
             ),
             //
             // List len
-            (
-                "test_pass_list_len",
-                "= list [ 1 2 3 ]\r\n= len List.len list",
-                "fn main() {\r\n    let list: Vec<i64> = vec![ 1, 2, 3 ];\r\n    let len: i64 = list.len() as i64;\r\n}\r\n",
-            ),
+            ("test_pass_list_len", "= list [ 1 2 3 ]\r\n= len List.len list", "fn main() {\r\n    let list: Vec<i64> = vec![ 1, 2, 3 ];\r\n    let len: i64 = list.len() as i64;\r\n}\r\n"),
+            /*
             //
-            // Function calls
+            // List map
+            (
+                "test_pass_list_map",
+                "= list [ 1 ]\r\n= mapfn \\ i64 i64 i => * i 100\r\n= mapped List.map list ( mapfn )",
+                "fn main() {\r\n    let list: Vec<i64> = vec![ 1 ];\r\n    fn mapfn(i: i64) -> i64 {\r\n        i * 100\r\n    }\r\n    let mapped: Vec<i64> = list.iter().map(mapfn).collect();\r\n}\r\n",
+            ),
+            */
+            //
+            // Basic arithmetic function calls
             ("test_pass_internal_function_calls_plus", "+ 1 2", "fn main() {\r\n    1 + 2;\r\n}\r\n"),
             ("test_pass_internal_function_calls_negative", "- 1.1 2.2", "fn main() {\r\n    1.1 - 2.2;\r\n}\r\n"),
             ("test_pass_internal_function_calls_multiply", "* 3 4", "fn main() {\r\n    3 * 4;\r\n}\r\n"),
@@ -123,27 +128,15 @@ impl IntegrationTests {
             ("test_pass_assignment_float", "= a 1.1", "fn main() {\r\n    let a: f64 = 1.1;\r\n}\r\n"),
             ("test_pass_assignment_float_neg", "= a -1.7976931348623157E+308", "fn main() {\r\n    let a: f64 = -1.7976931348623157E+308;\r\n}\r\n"),
             ("test_pass_assignment_arithmetic", "= a + 1 2", "fn main() {\r\n    let a: i64 = 1 + 2;\r\n}\r\n"),
-            (
-                "test_pass_assignment_internal_function_calls_with_references",
-                "= a + 1 2\r\n= b - 3 a",
-                "fn main() {\r\n    let a: i64 = 1 + 2;\r\n    let b: i64 = 3 - a;\r\n}\r\n",
-            ),
+            ("test_pass_assignment_internal_function_calls_with_references", "= a + 1 2\r\n= b - 3 a", "fn main() {\r\n    let a: i64 = 1 + 2;\r\n    let b: i64 = 3 - a;\r\n}\r\n"),
             //
             // Functions
             ("test_pass_nested_internal_function_call1", "= a - + 1 2 3", "fn main() {\r\n    let a: i64 = 1 + 2 - 3;\r\n}\r\n"),
             ("test_pass_nested_internal_function_call2", "= a / * - + 1 2 3 4 5", "fn main() {\r\n    let a: i64 = 1 + 2 - 3 * 4 / 5;\r\n}\r\n"),
             ("test_pass_nested_internal_function_call3", "= a + 1 * 3 2", "fn main() {\r\n    let a: i64 = 1 + 3 * 2;\r\n}\r\n"),
             ("test_pass_func_def_singleline1", "= a \\ i64 => 123", "fn main() {\r\n    fn a() -> i64 {\r\n        123 as i64\r\n    }\r\n}\r\n"),
-            (
-                "test_pass_func_def_singleline2",
-                "= a \\ i64 i64 arg1 => + 123 arg1",
-                "fn main() {\r\n    fn a(arg1: i64) -> i64 {\r\n        123 + arg1\r\n    }\r\n}\r\n",
-            ),
-            (
-                "test_pass_func_def_multiline1",
-                "= a \\ i64 i64 i64 arg1 arg2 =>\r\n+ arg1 arg2",
-                "fn main() {\r\n    fn a(arg1: i64, arg2: i64) -> i64 {\r\n        arg1 + arg2\r\n    }\r\n}\r\n",
-            ),
+            ("test_pass_func_def_singleline2", "= a \\ i64 i64 arg1 => + 123 arg1", "fn main() {\r\n    fn a(arg1: i64) -> i64 {\r\n        123 + arg1\r\n    }\r\n}\r\n"),
+            ("test_pass_func_def_multiline1", "= a \\ i64 i64 i64 arg1 arg2 =>\r\n+ arg1 arg2", "fn main() {\r\n    fn a(arg1: i64, arg2: i64) -> i64 {\r\n        arg1 + arg2\r\n    }\r\n}\r\n"),
             (
                 "test_pass_func_def_multiline2",
                 "= a \\ i64 i64 i64 i64 arg1 arg2 arg3 =>\r\n= x + arg1 arg2\r\n+ x arg3",
@@ -194,19 +187,77 @@ impl IntegrationTests {
                 "= a \\ ( i64 i64 ) i64 i64 arg1 arg2 =>\r\n arg1 arg2\r\n= b \\ i64 i64 arg3 => + 123 arg3\r\n= c a ( b ) 456",
                 "fn main() {\r\n    fn a(arg1: &dyn Fn(i64) -> i64, arg2: i64) -> i64 {\r\n        arg1(arg2)\r\n    }\r\n    fn b(arg3: i64) -> i64 {\r\n        123 + arg3\r\n    }\r\n    let c: i64 = a(&b, 456);\r\n}\r\n",
             ),
+            //
+            // Type inference
             (
                 "test_pass_type_inference_assign_to_constref",
                 "= a 123\r\n= aa a\r\n= aaa aa\r\n= aaaa aaa",
                 "fn main() {\r\n    let a: i64 = 123;\r\n    let aa: i64 = a;\r\n    let aaa: i64 = aa;\r\n    let aaaa: i64 = aaa;\r\n}\r\n",
             ),
             ("test_pass_type_inference_assign_to_funccall", "= a + 1 2", "fn main() {\r\n    let a: i64 = 1 + 2;\r\n}\r\n"),
-            /*
-            // List map
             (
-                "test_pass_list_map",
-                "= list [ 1 ]\r\n= mapfn \\ i64 i64 i => * i 100\r\n= mapped List.map list ( mapfn )",
-                "fn main() {\r\n    let list: Vec<i64> = vec![ 1 ];\r\n    fn mapfn(i: i64) -> i64 {\r\n        i * 100\r\n    }\r\n    let mapped: Vec<i64> = list.iter().map(mapfn).collect();\r\n}\r\n",
+                "test_pass_type_inference_assign_to_constref_of_funccall",
+                "= a + 1 2\r\n= aa a\r\n= aaa aa\r\n= aaaa aaa",
+                "fn main() {\r\n    let a: i64 = 1 + 2;\r\n    let aa: i64 = a;\r\n    let aaa: i64 = aa;\r\n    let aaaa: i64 = aaa;\r\n}\r\n",
             ),
+            (
+                "test_pass_fndef_return_statement",
+                "= a \\ i64 => ? == 1 1 1 0\r\na",
+                "fn main() {\r\n    fn a() -> i64 {\r\n        if 1 == 1 {\r\n            1\r\n        } else {\r\n            0\r\n        }\r\n    }\r\n    a();\r\n}\r\n",
+            ),
+            (
+                "test_pass_funccall_zero_args",
+                "//define function\r\n= a \\ i64 =>\r\n123\r\n\r\n//call function\r\na",
+                "fn main() {\r\n    //define function\r\n    fn a() -> i64 {\r\n        123 as i64\r\n    }\r\n    //call function\r\n    a();\r\n}\r\n",
+            ),
+            // TODO function call void/null/() return
+            (
+                "test_pass_funccall_one_arg",
+                "//define function\r\n= a \\ i64 i64 arg1 =>\r\narg1\r\n\r\n//call function\r\na 123",
+                "fn main() {\r\n    //define function\r\n    fn a(arg1: i64) -> i64 {\r\n        arg1\r\n    }\r\n    //call function\r\n    a(123);\r\n}\r\n",
+            ),
+            (
+                "test_pass_funccall_two_args_eval_internal_func_call",
+                "//define function\r\n= a \\ i64 i64 i64 arg1 arg2 =>\r\n+ arg1 arg2\r\n\r\n//call function\r\na + 123 456 789",
+                "fn main() {\r\n    //define function\r\n    fn a(arg1: i64, arg2: i64) -> i64 {\r\n        arg1 + arg2\r\n    }\r\n    //call function\r\n    a(123 + 456, 789);\r\n}\r\n",
+            ),
+            //
+            // Println
+            ("test_pass_println_int", "@ 1", "fn main() {\r\n    println!(\"{}\", 1);\r\n}\r\n"),
+            ("test_pass_println_float", "@ 1.23", "fn main() {\r\n    println!(\"{}\", 1.23);\r\n}\r\n"),
+            ("test_pass_println_string", "@ \"Hello, world\"", "fn main() {\r\n    println!(\"{}\", \"Hello, world\".to_string());\r\n}\r\n"),
+            ("test_pass_println_fn_call", "@ + 1 2", "fn main() {\r\n    println!(\"{}\", 1 + 2);\r\n}\r\n"),
+            ("test_pass_println_constantref", "= a 1\r\n@ a", "fn main() {\r\n    let a: i64 = 1;\r\n    println!(\"{}\", a);\r\n}\r\n"),
+            ("test_pass_println_constantref_twice", "= a 1\r\n= b a\r\n@ b", "fn main() {\r\n    let a: i64 = 1;\r\n    let b: i64 = a;\r\n    println!(\"{}\", b);\r\n}\r\n"),
+            ("test_pass_println_from_fn_def", "= a \\ i64 => 1\r\n@ a", "fn main() {\r\n    fn a() -> i64 {\r\n        1 as i64\r\n    }\r\n    println!(\"{}\", a());\r\n}\r\n"),
+            //
+            // If expressions / Ternary expressions
+            ("test_pass_if", "? true 1 0", "fn main() {\r\n    if true {\r\n        1\r\n    } else {\r\n        0\r\n    };\r\n}\r\n"),
+            (
+                "test_pass_if_assignment",
+                "= get_true \\ bool => true\r\n? get_true 1 0",
+                "fn main() {\r\n    fn get_true() -> bool {\r\n        true\r\n    }\r\n    if get_true() {\r\n        1\r\n    } else {\r\n        0\r\n    };\r\n}\r\n",
+            ),
+            (
+                "test_pass_if_fn_assignment",
+                "= get_truer \\ i64 bool arg1 => > arg1 5\r\n? get_truer 10 1 0",
+                "fn main() {\r\n    fn get_truer(arg1: i64) -> bool {\r\n        arg1 > 5\r\n    }\r\n    if get_truer(10) {\r\n        1\r\n    } else {\r\n        0\r\n    };\r\n}\r\n",
+            ),
+            /*
+            (TODO is valid output but has extra spaces - need to find way to remove Indents when If is used in an assignment)
+            ("test_pass_if_assignment_with_if_expr", "= a ? true 1 0", "fn main() {\r\n    let a: i64 =             if true {\r\n                1\r\n            } else {\r\n                0\r\n            };\r\n}\r\n"),
+            */
+            //
+            // Example Functions
+            (
+                "test_pass_example_fibonacci",
+                "= fibonacci \\ i64 i64 n => ? < n 2 1 + fibonacci - n 1 fibonacci - n 2\r\n@ fibonacci 10",
+                "fn main() {\r\n    fn fibonacci(n: i64) -> i64 {\r\n        if n < 2 {\r\n            1\r\n        } else {\r\n            fibonacci(n - 1) + fibonacci(n - 2)\r\n        }\r\n    }\r\n    println!(\"{}\", fibonacci(10));\r\n}\r\n",
+            ),
+            /* Loops - for loops - not required?
+            ("for_loops",
+                "= a \\ i64 i64 arg1 => + 123 arg1\r\n.. b 0 100\r\na b\r\n.",
+                "fn main() {\r\n    fn a(arg1: i64) -> i64 {\r\n        123 + arg1\r\n    }\r\n    for b in 0..100 {\r\n        a(b);\r\n    }\r\n}\r\n")
             */
         ];
         let tests = test_strs.iter().map(|(a, b, c)| (a.to_string(), b.to_string(), c.to_string())).collect::<Vec<(TestName, ToylangCodeInput, ExpectedRustCodeOutput)>>();
